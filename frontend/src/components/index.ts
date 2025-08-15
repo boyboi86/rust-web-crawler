fa0ba6896c7212b1f2905149274f50bd875fbf7b
this.wx <boyboi86@gmail.com>
2025-08-15 15:41:10 +0800

    update higher component lvl

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,5 @@
 // Export all components from their respective folders
-export * from './ui'
-export * from './forms'
-export * from './layout'
-export * from './features'
+export * from './ui';
+export * from './forms';
+export * from './layout';
+export * from './features';
